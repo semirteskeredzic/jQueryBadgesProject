93444b4e05942c1361d0332a25ba2b0bf26359ce
Semir <semir.teskeredzic@gmail.com>
2017-08-19 15:23:46 +0200

    added addCourse function

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,11 +3,24 @@ $(function() {
     url: 'https://www.codeschool.com/users/Endocannabinoid.json',
     dataType: 'jsonp',
     success: function(response){
-      //handle response
+      console.log('response', response.courses.completed);
+      addCourses(response.courses.completed);
     }
 
-  })
+  });
 
+  function addCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $('<div />',  {
+        'class': 'course'
+      }).appendTo($badges);
+    });
+
+  }
 
   // your code will go here
 
