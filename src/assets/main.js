7cf1af0bd0ed9dc5fc619efc9c65177800a9b810
Semir <semir.teskeredzic@gmail.com>
2017-08-19 15:11:53 +0200

    changed js and index

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,4 +1,13 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/Endocannabinoid.json',
+    dataType: 'jsonp',
+    success: function(response){
+      //handle response
+    }
+
+  })
+
 
   // your code will go here
 
